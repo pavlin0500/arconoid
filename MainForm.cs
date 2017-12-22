744981dbacf2c567831b656ce916782167692dc3
pavlin0500 <32095279+pavlin0500@users.noreply.github.com>
2017-12-22 10:18:03 +0300

    mainForm

---
+++ MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Linq;
+using WindowsFormsApplication341.Model;
+
+namespace WindowsFormsApplication341
+{
+    public partial class MainForm : Form
+    {
+        private Game game;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            game = new Game();
+            game.Bounds = pnGame.ClientRectangle;
+            game.InitLevel();
+            pnGame.Game = game;
+
+            var tm = new Timer{Enabled = true, Interval = 20};
+            tm.Tick += delegate
+            {
+                var dt = 0.1f;
+                game.Update(dt);
+                pnGame.Invalidate();
+
+                if (game.IsGameOver())//мяч потерян
+                {
+                    tm.Stop();
+                    MessageBox.Show("Game over!");
+                    game.InitLevel();//начало новой игры
+                    tm.Start();
+                }
+
+                if (game.IsLevelCompleted())//уровень пройден?
+                {
+                    tm.Stop();
+                    game.NextLevelNumber++;//след уровень
+                    game.InitLevel();
+                    tm.Start();
+                }
+            };
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
